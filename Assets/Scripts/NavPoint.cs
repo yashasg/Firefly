48ba75ab9f9a461804c7cc720bd1ca01b2bc8fe8
samratnagarjuna <samratnagarjuna@gmail.com>
2016-08-06 19:47:07 -0600

    Added NavMesh Agent

---
+++ Assets/Scripts/NavPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+public class NavPoint : MonoBehaviour {
+
+	private MeshRenderer m_renderer;
+
+	// Use this for initialization
+	void Start () {
+		m_renderer = GetComponent<MeshRenderer>();
+		m_renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
