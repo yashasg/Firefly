64b8971eb9eb03c2d1fd421f2534c18a66f15c82
u1006341 <u1006341@umail.utah.edu>
2016-08-06 18:50:22 -0600

    made hover script.destroys object on hover

---
+++ Assets/FireflyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyMovement : MonoBehaviour {
+    public GameObject m_Firefly;
+    public float m_speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Firefly.transform.Translate(Vector2.right*Time.deltaTime*m_speed);
+	}
+}
