bba65cb1b8cb061693e352e83466399f26c17693
u1006341 <u1006341@umail.utah.edu>
2016-08-07 21:32:56 -0600

    added sounds folder

--- Assets/Scripts/HoverSensor.cs
+++ Assets/Scripts/HoverSensor.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class HoverSensor : MonoBehaviour {
     float m_timercatcher;
     public float m_hovertime;
-    public AudioSource m_sound;
+    AudioSource m_sound;
+    public GameObject my_Firefly;
+    public GameObject ParticleEffect;
+    bool isInvoked = false;
 
 	// Use this for initialization
 	void Start () {
         m_sound = GetComponent<AudioSource>();
+        ParticleEffect.SetActive(true);
 
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	        
 	}
     void OnMouseOver()
     {
@@ -24,18 +29,23 @@ public class HoverSensor : MonoBehaviour {
             m_timercatcher = 0.0f;
             AudioSource.PlayClipAtPoint(m_sound.clip, Camera.main.gameObject.transform.position);
             //m_sound.PlayOneShot();
-           // JarManager.Instance.AddFireFly();
-           // sendToJar();
-           
-           Destroy(gameObject);
+           sendToJar();
+          //  Invoke("makeNewFly", 1.0f);
+           // Destroy(GetComponent<Movement>());
         }
         else
             m_timercatcher += Time.deltaTime;
     }
+    void makeNewFly()
+    {
+        Instantiate(my_Firefly);
+     
+    }
 
     void sendToJar()
     {
-       Vector3 dest = JarManager.Instance.GetTargetJarPosition();
+        JarManager.Instance.AddFireFly();
+        Vector3 dest = JarManager.Instance.GetTargetJarPosition();
         gameObject.transform.position.Set(dest.x,dest.y,dest.z);
 
     }
---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    float m_timer;
+    float gameTimer;
+	// Use this for initialization
+	void Start () {
+        m_timer = PlayerPrefs.GetFloat("Timer");
+        gameTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_timer >= gameTimer)
+        {
+            SceneManager.LoadScene(0);
+        }
+	
+	}
+}
